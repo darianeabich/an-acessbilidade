0b92b12804bef2f316f41e45f1d9fcaffb874d6e
Dariane Abich <darianeabich@gmail.com>
2021-06-11 18:02:15 -0300

    Movimentação na tela através dos botões 'setas' do teclado

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.module.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { YesNoButtonGroupComponent } from './yes-no-button-group.component';
+import { KeyboardManagerModule } from '../../directives/keyboard-manager/keyboard-manager.module';
 
 
 
@@ -10,6 +11,7 @@ import { YesNoButtonGroupComponent } from './yes-no-button-group.component';
   ],
   imports: [
     CommonModule,
+    KeyboardManagerModule,
   ],
   exports: [
     YesNoButtonGroupComponent
---
+++ src/app/shared/directives/keyboard-manager/keyboard-managed-item.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appKmItem]'
+})
+export class KeyboardManagedItemDirective {
+
+  constructor(private elementRef: ElementRef<HTMLElement>) {}
+
+  public focus(): void {
+    this.elementRef.nativeElement.focus();
+  }
+
+  public isFocused(): boolean {
+    return this.elementRef.nativeElement === document.activeElement;
+  }
+}
