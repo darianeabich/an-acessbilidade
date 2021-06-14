a39a2b3dc32be152329b0a32d8dda0292084054a
Dariane Abich <darianeabich@gmail.com>
2021-06-14 08:27:38 -0300

    Adequações para o estado  disabled

---
+++ src/app/shared/directives/disable-control/disable-control.module.ts
@@ -0,0 +1,16 @@
+import { DisableControlDirective } from './disable-control.directive';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+
+@NgModule({
+  declarations: [
+    DisableControlDirective,
+  ],
+  imports: [
+    CommonModule,
+  ],
+  exports:[
+    DisableControlDirective,
+  ]
+})
+export class DisableControlModule {}
